32679a3ea3b047664397873ad948eb9d6b82c741
BraianHeinrich <braianmarceloh@gmail.com>
2025-08-07 17:56:01 -0300

    ultima

--- assets/index.js
+++ assets/index.js
@@ -232,6 +232,24 @@ form.addEventListener('submit', function(event){
   }
 });
 
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToTopBtn.classList.add("show");
+  } else {
+    scrollToTopBtn.classList.remove("show");
+  }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+
 /*ANIMACION */
 const animatedSections = document.querySelectorAll('.animate-in');
 
